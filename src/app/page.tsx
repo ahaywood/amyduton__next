adf6a4eb80cc39adc51b63270cbcb713cf0507a6
Amy Haywood Dutton <me@amyhaywood.com>
2024-05-05 21:46:21 -0500

    🔥 Removed link to download my CV

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -257,14 +257,6 @@ function Resume() {
           <Role key={roleIndex} role={role} />
         ))}
       </ol>
-      <Button
-        href="https://www.dropbox.com/scl/fi/fedqwqct46qg1kt2dzh02/CV-DUTTON-AMY.pdf?rlkey=22rvtqrz5vlkbfcfukwshwi24&dl=0"
-        variant="secondary"
-        className="group mt-6 w-full"
-      >
-        Download CV
-        <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
-      </Button>
     </div>
   )
 }
